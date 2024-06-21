f8d5915c8d2d8e487bf98b78bafa2e2079588e59
Gargi-Shah <143215721+Gargi-Shah@users.noreply.github.com>
2024-06-21 14:08:10 +0530

    Add files via upload

---
+++ Task-2/script2.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
